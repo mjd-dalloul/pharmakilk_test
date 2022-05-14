baecb116760c091ee2c4bc433fef4715ffaa0e75
mjd <majed.dalloul79@gmail.com>
2022-05-14 19:25:48 +0300

    base app

---
+++ lib/core/app/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppProvider extends StatelessWidget {
+  const AppProvider({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
