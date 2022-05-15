d975644eb0c25a6a51187b4ccbf476c6dfb7806c
mjd <majed.dalloul79@gmail.com>
2022-05-15 13:13:28 +0300

    splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:pharmaklik/core/app/app.dart';
 import 'package:pharmaklik/injection.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(
+    widgetsBinding: WidgetsFlutterBinding.ensureInitialized(),
+  );
   await inject();
   runApp(const MyApp());
+  FlutterNativeSplash.remove();
 }
